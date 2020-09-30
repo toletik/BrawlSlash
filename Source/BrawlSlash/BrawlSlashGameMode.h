59ff8e47d547cd4b0b7b3b3ff604d2bfbe4a4aa7
todorov.anatole <todorov.anatole@gmail.com>
2020-09-30 11:09:47 +0200

    Add a Blank Project

---
+++ Source/BrawlSlash/BrawlSlashGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BrawlSlashGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABrawlSlashGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABrawlSlashGameMode();
+};
+
+
+
