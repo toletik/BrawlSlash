fe87bb5405ffee9e0ca3668c26632ac3de10c8f7
D_Nicolas <n.deprez@student.isartdigital.com>
2020-10-06 08:44:02 +0200

    Notify to idle

---
+++ Source/BrawlSlash/AnimNotify/AnimNotify_ToIdle.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_ToIdle.h"
+#include "../Character_Base.h"
+#include "Components/SkeletalMeshComponent.h"
+
+void UAnimNotify_ToIdle::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	ACharacter_Base* character = Cast<ACharacter_Base>(MeshComp->GetOwner());
+
+	if (character)
+		character->state = E_STATE::IDLE;
+}
