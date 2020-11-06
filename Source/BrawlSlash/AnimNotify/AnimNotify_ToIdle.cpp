01029be2bdc63c957e926647596f4bf7196590f9
Nicolas <n.deprez@student.isartdigital.com>
2020-11-06 21:01:32 +0100

    Continue of fixing new dash system Add comments in player's function of dash and attack

--- Source/BrawlSlash/AnimNotify/AnimNotify_ToIdle.cpp
+++ Source/BrawlSlash/AnimNotify/AnimNotify_ToIdle.cpp
@@ -5,17 +5,16 @@
 #include "../Characters/Character_Base.h"
 #include "../Characters/Character_Player.h"
 #include "Components/SkeletalMeshComponent.h"
-#include "Animation/AnimSequenceBase.h"
 
 void UAnimNotify_ToIdle::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
 {
 	ACharacter_Base* character = Cast<ACharacter_Base>(MeshComp->GetOwner());
 
-	ACharacter_Player* player = Cast<ACharacter_Player>(character);
-
-	if (player && player->needToAttack)
-		player->Attack();
-
 	if (character)
 		character->state = E_STATE::IDLE;
+
+	if (Cast<ACharacter_Player>(MeshComp->GetOwner()))
+	{
+		GEngine->AddOnScreenDebugMessage(-21, 1.0f, FColor::Red, "TOIDLE");
+	}
 }
--- Source/BrawlSlash/Characters/Character_Player.h
+++ Source/BrawlSlash/Characters/Character_Player.h
@@ -47,13 +47,13 @@ class BRAWLSLASH_API ACharacter_Player : public ACharacter_Base
 	int dashHitDamage = 4;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = "true"))
-	float stickPointFight = 100.0f;
+	float stickPointFight = 200.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = "true"))
-	float stickPointNav = 150.0f;
+	float stickPointNav = 200.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = "true"))
-	float stickRangeFight = 300.0f;
+	float stickRangeFight = 350.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = "true"))
 	float stickRangeNav = 350.0f;
@@ -64,7 +64,7 @@ class BRAWLSLASH_API ACharacter_Player : public ACharacter_Base
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = "true"))
 	float maxDistanceToDashNav = 10000.0f;
 
-	FVector dashHitPointOnFocus = FVector::ZeroVector;
+	FVector dashHitImpactPoint = FVector::ZeroVector;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Mobility Points", meta = (AllowPrivateAccess = "true"))
 	float preparingDashDuration = 0.3f;
@@ -144,6 +144,8 @@ public:
 	void Attack();
 
 	bool needToAttack = false;
+
+	bool isAtEndOfAttackAnim = false;
 
 	FTimerHandle comboTimer;
 
